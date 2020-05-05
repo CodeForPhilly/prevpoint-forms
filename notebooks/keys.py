fa21b59af68bdc493eab4bde70cea37149b870a7
Faith <faith.benamy@gmail.com>
2020-05-05 17:53:25 -0400

    added prototypes

---
+++ notebooks/keys.py
@@ -0,0 +1,4 @@
+#azure endpoint
+COMPUTER_VISION_ENDPOINT = "https://prev-forms.cognitiveservices.azure.com"
+#azure subscription key
+COMPUTER_VISION_SUBSCRIPTION_KEY = "3717bd5528c74754b213389d4c24351c"
