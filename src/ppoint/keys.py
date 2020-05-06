ac365a30fc3aa29b844d38c1b27b6036fd621232
Faith <faith.benamy@gmail.com>
2020-05-06 09:26:03 -0400

    fixed git ignore issues

--- src/ppoint/keys.py
+++
@@ -1,4 +0,0 @@
-#azure endpoint
-COMPUTER_VISION_ENDPOINT = "https://prev-forms.cognitiveservices.azure.com"
-#azure subscription key
-COMPUTER_VISION_SUBSCRIPTION_KEY = "be9c74bd9742453d8afbccb978f30075"
