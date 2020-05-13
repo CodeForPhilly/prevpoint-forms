7892e0d2cb0a5a361f0eb4fef6d73743daa3b00b
Faith <faith.benamy@gmail.com>
2020-05-13 17:53:28 -0400

    working prototypes

---
+++ notebooks/keys.py
@@ -0,0 +1,4 @@
+#azure endpoint
+COMPUTER_VISION_ENDPOINT = "https://prev-forms.cognitiveservices.azure.com"
+#azure subscription key
+COMPUTER_VISION_SUBSCRIPTION_KEY = "5396b2a7b90c47e98a0e9a25f0f69581"
---
+++ src/ppoint/keys.py
@@ -0,0 +1,4 @@
+#azure endpoint
+COMPUTER_VISION_ENDPOINT = "https://prev-forms.cognitiveservices.azure.com"
+#azure subscription key
+COMPUTER_VISION_SUBSCRIPTION_KEY = "be9c74bd9742453d8afbccb978f30075"
